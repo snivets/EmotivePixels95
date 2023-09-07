85b6d77d70e58ddf4c6e5a9c1979d73f9f4adbbf
snivets <equinox.nls@gmail.com>
2023-09-07 14:31:10 -0400

    Use CSS Grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
-import { useAsyncFn } from 'react-use';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
 
-import { Button, Separator, styleReset } from 'react95';
+import { Button, Frame, GroupBox, Radio, Select, styleReset } from 'react95';
 // pick a theme of your choice
 import original from 'react95/dist/themes/original';
 // original Windows95 font (optionally)
 import ms_sans_serif from 'react95/dist/fonts/ms_sans_serif.woff2';
 import ms_sans_serif_bold from 'react95/dist/fonts/ms_sans_serif_bold.woff2';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import notes from './assets/episode-notes.json';
 
 const EP_FEED_URL = 'https://anchor.fm/s/4cba81a4/podcast/rss';
+const POD_TITLE = "Emotive Pixels: Videogame Deep Dives";
 
 function getTitles(feedRss: string) {
-  return new window.DOMParser().parseFromString(feedRss, 'text/xml').querySelectorAll("title");
+  var titlesInclusive = new window.DOMParser()
+    .parseFromString(feedRss, 'text/xml')
+    .querySelectorAll("title");
+  return Array.from(titlesInclusive)
+    .map(t => t.textContent)
+    .filter(val => val !== '' && val !== POD_TITLE);
+}
+
+function getNotes() {
+  console.log(notes[0].notes);
 }
 
 const GlobalStyles = createGlobalStyle`
@@ -29,38 +39,86 @@ const GlobalStyles = createGlobalStyle`
     font-weight: bold;
     font-style: normal
   }
-  body {
+  body, textarea {
     font-family: 'ms_sans_serif';
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
   }
+  #grid-wrapper {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(4, 1fr);
+    gap: 10px;
+    width: 100vw;
+    height: 100vh;
+    align-items: end;
+  }
+  .row-one { grid-row: 1; }
+  .row-two { grid-row: 2; }
+  .row-four { grid-row: 4; }
+  .full-width { grid-column: 2 / 3; }
 `;
 
 const App = () => {
-  const [titles, setTitles] = useState<string[]>([]);
+  const [titles, setTitles] = useState<any[]>([]);
+  const [episodeText, setEpisodeText] = useState<string>('Episode info will appear here - pick an episode!');
+
+  var titleList: any[] = [];
 
   const fetchData = async () => {
     try {
       const response = await fetch(EP_FEED_URL);
       const result = await response.text();
-      const titleElements = getTitles(result);
+      const titles = getTitles(result);
 
-      const titleArray = Array.from(titleElements).map((titleElement) => {
-        return titleElement.textContent || '';
+      titles.forEach(function(element) {
+        titleList.push({ label: element, value: element })
       });
 
-      setTitles(titleArray);
+      setTitles(titleList);
+
+      getNotes();
+      
     } catch (error) {
       console.error('Error fetching data:', error);
     }
   };
 
+  useEffect(() => {
+    fetchData();
+  }, [])
+
   return (
-    <div>
+    <>
       <GlobalStyles />
       <ThemeProvider theme={original}>
-        <Button onClick={fetchData}>Get data</Button>
-        <textarea rows={10} cols={50} value={titles.join('\n')} readOnly />
+        <div id='grid-wrapper'>
+          <Button className='row-four full-width'>Start menu placeholder!</Button>   
+          <div className='row-two full-width'>
+            <GroupBox label="What to show">
+              <div>
+                <Select
+                  options={titles}
+                  menuMaxHeight={250}
+                  width={400}
+                  onChange={e => setEpisodeText(e.value)} />
+              </div>
+              <div>
+                <Radio value={'d'} label={'Description'} className='row-two' />
+                <Radio value={'i'} label={'Insights'} className='row-two' />
+              </div>
+            </GroupBox>
+            <Frame
+              variant='outside'
+              shadow
+              style={{ padding: '0.5rem', lineHeight: '1.5', width: 600 }}>
+                {episodeText}
+            </Frame>
+          </div>
+        </div>
       </ThemeProvider>
-    </div>
+    </>
   );
 };
 
