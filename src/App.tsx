b4a66a505686649bed79bd5474085e5f3b6f8b59
snivets <equinox.nls@gmail.com>
2023-09-22 00:07:16 -0400

    Migrate to React95/core and correct stylings
    Lots of icky icky divs... what is this, Angular?!

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,22 @@
 import { useEffect, useMemo, useState } from 'react';
 import DOMPurify from 'dompurify';
 
-import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import {
-  Button,
-  GroupBox,
-  Radio,
-  Select,
-  styleReset,
-  AppBar,
-  Toolbar,
-  MenuList,
-  MenuListItem,
-  Separator,
-  TextInput,
-  WindowHeader,
-  Window,
-  WindowContent,
-  ScrollView
-} from 'react95';
-import original from 'react95/dist/themes/original';
-import ms_sans_serif from 'react95/dist/fonts/ms_sans_serif.woff2';
-import ms_sans_serif_bold from 'react95/dist/fonts/ms_sans_serif_bold.woff2';
+  ThemeProvider,
+  TaskBar,
+  List,
+  Modal,
+  Dropdown,
+  RadioButton,
+  Frame,
+  Fieldset
+} from '@react95/core';
+import '@react95/icons/icons.css';
 
 import notes from './assets/episode-notes.json';
 import logoImg from './assets/ep-icon.png';
 import './assets/desktop-styling.css';
+import { Computer, ReaderClosed, WindowsExplorer } from '@react95/icons';
 
 const EP_FEED_URL = 'https://anchor.fm/s/4cba81a4/podcast/rss';
 const POD_TITLE = "Emotive Pixels: Videogame Deep Dives";
@@ -41,96 +32,56 @@ const CRAIG_BIO = "Craig is a Coloradan in all senses of the term, except that h
 const WILL_BIO = "Will is a genius, the biggest and boldest gamer among us (and by that I mean he could complete Factorio the quickest and loves FTL the most), and shows up whenever he feels like it. Unfortunately, we often disappoint him with our choice of games - either too obscure, or insufficiently GAMEY. He loves systems, JRPGs, and Brandon Sanderson. He resides in Seattle, the original hub of the podcast, and in fact lived for a time above the Uwajimaya in the very room where the early episodes were recorded.";
 const PAULY_BIO = "Pauly is the friendly likeable one of the lot, a charming man from New England who loves music, plays music, and is picking up DJing. He likes many kinds of games in no particular pattern that I can discern, but suffice it to say he has great taste. He lives in Syracuse, NY.";
 
-const GlobalStyles = createGlobalStyle`
-  ${styleReset}
-  @font-face {
-    font-family: 'ms_sans_serif';
-    src: url('${ms_sans_serif}') format('woff2');
-    font-weight: 400;
-    font-style: normal
-  }
-  @font-face {
-    font-family: 'ms_sans_serif';
-    src: url('${ms_sans_serif_bold}') format('woff2');
-    font-weight: bold;
-    font-style: normal
-  }
-  body, textarea {
-    font-family: 'ms_sans_serif';
-    box-sizing: border-box;
-    padding: 0;
-    margin: 0;
-  }
-`;
-
 function CharacterModal(props: any) {
+  console.log('clicked');
+
   return (
-    <Window className='window half-width pauly-dialog'>
-      <WindowHeader active={true} className='window-title'>
-        <span>
-          {props.title}
-        </span>
-        <Button onClick={props.clickFunc}>
-          <span className='close-icon' />
-        </Button>
-      </WindowHeader>
-      <WindowContent>
-        {props.bio}
-      </WindowContent>
-    </Window>
+    <Modal
+      width="300"
+      height="auto"
+      icon={<Computer variant="32x32_4" />}
+      title={props.title}
+      defaultPosition={{x: 35, y: 40}}
+      closeModal={props.clickFunc}
+      // buttons={[{
+      //   value: 'Ok',
+      //   onClick: props.clickFunc
+      // }]}
+      menu={[{
+        name: 'File',
+        list: <List>
+          <List.Item onClick={props.toggleFunc}>Toggle Mode</List.Item>
+        </List>
+      }]}
+    >{props.bio}</Modal>
   );
 }
 
 // Props: open (state), open, funcs for each person's dialog
 function EPStartMenu(props: any) {
   return (
-    <AppBar className='row-four full-width'>
-      <Toolbar style={{ justifyContent: 'space-between' }}>
-        <div style={{ position: 'relative', display: 'inline-block' }}>
-          <Button
-            onClick={() => props.setOpen(!props.open)}
-            active={props.open}
-            style={{ fontWeight: 'bold' }}
-          >
-            <img src={logoImg} alt='EP logo' style={{ height: '20px', marginRight: 4 }} />
-            Start
-          </Button>
-          {props.open && (
-            <MenuList
-              style={{
-                position: 'absolute',
-                left: '0',
-                top: '100%'
-              }}
-              onClick={() => props.setOpen(false)}
-            >
-              <MenuListItem onClick={props.nateModal}>
-                <span role='img' aria-label={NATE_TITLE}>👱</span>
-                Nate Stevens
-              </MenuListItem>
-              <MenuListItem onClick={props.willModal}>
-                <span role='img' aria-label={WILL_TITLE}>👨‍💻</span>
-                Will Atkinson
-              </MenuListItem>
-              <MenuListItem onClick={props.craigModal}>
-                <span role='img' aria-label={CRAIG_TITLE}>🧗‍♂️</span>
-                Craig Schuemann
-              </MenuListItem>
-              <MenuListItem onClick={props.paulyModal}>
-                <span role='img' aria-label={PAULY_TITLE}>🧞‍♂️</span>
-                Pauly Kroll
-              </MenuListItem>
-              <Separator />
-              <MenuListItem disabled>
-                <span role='img' aria-label='about'>ℹ️</span>
-                About EP
-              </MenuListItem>
-            </MenuList>
-          )}
-        </div>
-        <TextInput placeholder='Search episodes...' width={150} />
-      </Toolbar>
-    </AppBar>
+    <TaskBar list={<List>
+      <List.Item
+        icon={<ReaderClosed variant="32x32_4" />}
+        onClick={props.nateModal} >
+        {NATE_TITLE}
+      </List.Item>
+      <List.Item
+        icon={<WindowsExplorer variant="32x32_4" />}
+        onClick={props.craigModal} >
+        {CRAIG_TITLE}
+      </List.Item>
+      <List.Item
+        icon={<WindowsExplorer variant="32x32_4" />}
+        onClick={props.willModal} >
+        {WILL_TITLE}
+      </List.Item>
+      <List.Item
+        icon={<WindowsExplorer variant="32x32_4" />}
+        onClick={props.paulyModal} >
+        {PAULY_TITLE}
+      </List.Item>
+    </List>} />
   );
 }
 
@@ -195,6 +146,7 @@ const App = () => {
       .filter(val => val !== '' && val !== POD_TITLE);
   }
 
+  // Get the string that describes an episode's position, e.g. S3E10
   const getEpisodeSeasonString = (title: string) => {
     var e = findEpisodeXmlItem(title);
     var epId = 'bonus'; //default case
@@ -236,13 +188,12 @@ const App = () => {
 
   // When we get the RSS feed XML back
   useEffect(() => {
-    if (feedRssRaw) {
+    if (feedRssRaw && !selectedEpisodeTitle) {
       // Populate the dropdown with episode titles
-      const titles = getTitles();
-      const updatedTitles = titles.map(element => ({ label: element, value: element }));
-      setTitles(updatedTitles);
+      let titles = getTitles();
+      setTitles(titles);
       setSelectedRadio('d');
-      setSelectedEpisodeTitle(updatedTitles[0].label ?? ''); //populate dropdown with most recent episode
+      setSelectedEpisodeTitle(titles[0] ?? ''); //populate dropdown with most recent episode
     }
   }, [feedRssRaw]);
 
@@ -264,45 +215,45 @@ const App = () => {
 
   return (
     <>
-      <GlobalStyles />
-      <ThemeProvider theme={original}>j
+      <ThemeProvider>
         <div id='grid-wrapper'> 
-          <Window className='row-two half-width'>
-            <WindowContent>
-              <GroupBox label="Episode insights">
-                <div>
-                  <Select
+          <div className='row-two half-width'>
+            <Frame padding={4}>
+              <Frame boxShadow="in" padding={8} margin={2}>
+                <Fieldset legend="Episode insights">
+                  <Dropdown
                     options={titles}
-                    menuMaxHeight={250}
-                    width={400}
-                    value={selectedEpisodeTitle}
-                    onChange={e => getEpisodeSeasonString(e.value as string) } />
-                </div>
-                <div className="text-toggle-group">
-                  <Radio
-                    value={'d'}
-                    label={'Description'}
-                    className='row-two'
-                    checked={selectedRadio === 'd'}
-                    onChange={() => setSelectedRadio('d')}
-                  />
-                  <Radio
-                    value={'i'}
-                    label={'Insights'}
-                    className='row-two'
-                    checked={selectedRadio === 'i'}
-                    onChange={() => setSelectedRadio('i')}
-                    disabled={ getEpisodeInsightFromEpisodeId(selectedEpisodeId) === NO_INSIGHT }
-                  />
-                </div>
-              </GroupBox>
-            </WindowContent>
-            <ScrollView style={{ maxHeight: '200px' }}>
-              <div
-                style={{ padding: '0.5rem', lineHeight: '1.5', width: 500, maxHeight: '150px' }}
-                dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(episodeText)}} />
-            </ScrollView>
-          </Window>
+                    defaultValue={selectedEpisodeTitle}
+                    onChange={e => getEpisodeSeasonString(e.currentTarget.value) } />
+                  <div className="toggle-options" style={{margin: '12px 0 -12px 0'}}>
+                    {/* TODO: Why is this div necessary? */}
+                    <div style={{width: '22%'}}>
+                      <RadioButton
+                        value={'d'}
+                        className='row-two'
+                        checked={selectedRadio === 'd'}
+                        onChange={() => setSelectedRadio('d')}>
+                        Description
+                      </RadioButton>
+                    </div>
+                    <RadioButton
+                      value={'i'}
+                      className='row-two'
+                      checked={selectedRadio === 'i'}
+                      onChange={() => setSelectedRadio('i')}
+                      disabled={ getEpisodeInsightFromEpisodeId(selectedEpisodeId) === NO_INSIGHT }>
+                      Insights    
+                    </RadioButton>
+                  </div>
+                </Fieldset>
+                <Frame style={{ maxHeight: '150px', width: 500, overflowY: 'auto', padding: '0.5rem', margin: '16px 2px 2px 2px' }}>
+                  <div 
+                    style={{ lineHeight: '1.1', margin: '-16px 0'}}
+                    dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(episodeText)}} />
+                </Frame>
+              </Frame>
+            </Frame>
+          </div>
           <EPStartMenu
             open={open}
             setOpen={setOpen}
@@ -312,24 +263,28 @@ const App = () => {
             paulyModal={setPaulyOpen} />
           {nateOpen && (
             <CharacterModal
+              isOpen={nateOpen}
               clickFunc={() => { setNateOpen(false); }}
               title={NATE_TITLE}
               bio={NATE_BIO} />
           )}
           {craigOpen && (
             <CharacterModal
+              isOpen={craigOpen}
               clickFunc={() => { setCraigOpen(false); }}
               title={CRAIG_TITLE}
               bio={CRAIG_BIO} />
           )}
           {willOpen && (
             <CharacterModal
+              isOpen={willOpen}
               clickFunc={() => { setWillOpen(false); }}
               title={WILL_TITLE}
               bio={WILL_BIO} />
           )}
           {paulyOpen && (
             <CharacterModal
+              isOpen={paulyOpen}
               clickFunc={() => { setPaulyOpen(false); }}
               title={PAULY_TITLE}
               bio={PAULY_BIO} />
