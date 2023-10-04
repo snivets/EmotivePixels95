8e7553f6ee83d6c5ec931d48dc4651408a228e04
snivets <equinox.nls@gmail.com>
2023-10-04 19:39:00 -0400

    Styling nitpicks (how were these not defaults?)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { StyleSheetManager } from 'styled-components'
-import isPropValid from '@emotion/is-prop-valid'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <StyleSheetManager shouldForwardProp={isPropValid}>
-    <App />
-  </StyleSheetManager>,
+  <App />,
 )
