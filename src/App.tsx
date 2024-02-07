0c51d29910e9d9b4c62ad7e40929094d5a35a92b
snivets <equinox.nls@gmail.com>
2024-02-07 18:26:00 -0500

    feat: episode player

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import DOMPurify from 'dompurify';
 import {
   ThemeProvider,
@@ -7,7 +7,6 @@ import {
   RadioButton,
   Frame,
   Fieldset,
-  Video,
   Button
 } from '@react95/core';
 import '@react95/icons/icons.css';
@@ -25,20 +24,20 @@ import useEpisodeSeasonString from './hooks/useEpisodeSeasonString.tsx';
 import useEpisodeTitles from './hooks/useEpisodeTitles.tsx';
 import useEpisodeDescription from './hooks/useEpisodeDescription.tsx';
 import useRssEpisodeFinder from './hooks/useRssEpisodeFinder.tsx';
+import ReactPlayer from 'react-player/file';
 
 const App = () => {
   const [titles, setTitles] = useState<any[]>([]);
   const [episodeText, setEpisodeText] = useState<string>('Episode info will appear here - pick an episode!');
-  const [selectedEpisodeId, setSelectedEpisodeId] = useState<string>('');
-  const [selectedEpisodeTitle, setSelectedEpisodeTitle] = useState<string>('');
-  const [selectedEpisodeFile, setSelectedEpisodeFile] = useState<string>('');
-  const [selectedRadio, setSelectedRadio] = useState<string>('');
+  const [currentId, setCurrentId] = useState<string>('');
+  const [currentTitle, setCurrentTitle] = useState<string>('');
+  const [currentFile, setCurrentFile] = useState<string>('');
+  const [currentRadio, setCurrentRadio] = useState<string>('');
   const [insightsEnabled, setInsightsEnabled] = useState<boolean>(false);
   const [feedRss, setFeedRssRaw] = useState<string>('');
   // we're gonna need the element `link` and `itunes:image`
 
-  // Load the podcast RSS feed as soon as the page loads
-  const fetchData = async () => {
+  const fetchRssFeed = async () => {
     try {
       const response = await fetch(strings.EP_FEED_URL);
       setFeedRssRaw(await response.text());
@@ -47,8 +46,8 @@ const App = () => {
     }
   };
 
-  // Get the string that describes an episode's position, e.g. S3E10
-  const setEpisodeSeasonString = (title: string) => {
+  // Update all relevant text strings to match current episode
+  const updateSelectedEpisodeText = (title: string) => {
     var epId = useEpisodeSeasonString(title, parsedFeed);
 
     if (epId == '') {
@@ -56,31 +55,35 @@ const App = () => {
       return;
     }
 
-    setSelectedEpisodeId(epId);
-    setSelectedEpisodeTitle(title);
+    setCurrentId(epId);
+    setCurrentTitle(title);
     setInsightsEnabled(useEpisodeInsight(epId) !== strings.NO_INSIGHT);
   }
 
-  const loadPlayer = async () => {
-    var xml = useRssEpisodeFinder(selectedEpisodeTitle, parsedFeed);
+  const updateUrlForPlayer = async () => {
+    if (playerOpen) {
+      setPlayerOpen(false);
+      return;
+    }
+    var xml = useRssEpisodeFinder(currentTitle, parsedFeed);
     if (xml) {
       var url = xml.getElementsByTagName("enclosure")[0].getAttribute('url');
       if (url) {
-        setSelectedEpisodeFile(url);
+        setCurrentFile(url);
         setPlayerOpen(true);
       }
     }
   }
 
-  const updateFrameInfo = async () => {
+  const toggleDescriptionOrInsight = async () => {
     // Update the episode text based on the selectedRadio value
-    if (selectedRadio === 'd') {
+    if (currentRadio === 'd') {
       // Display description
-      const episodeText = useEpisodeDescription(selectedEpisodeTitle, parsedFeed);
+      const episodeText = useEpisodeDescription(currentTitle, parsedFeed);
       setEpisodeText(episodeText);
-    } else if (selectedRadio === 'i') {
+    } else if (currentRadio === 'i') {
       // Display insights
-      setEpisodeText(useEpisodeInsight(selectedEpisodeId));
+      setEpisodeText(useEpisodeInsight(currentId));
     }
   }
 
@@ -91,33 +94,33 @@ const App = () => {
 
   // Get podcast XML data on page load
   useEffect(() => {
-    fetchData();
+    fetchRssFeed();
   }, []);
 
   // When we get the RSS feed XML back, select the first item in the list
   useEffect(() => {
-    if (feedRss && !selectedEpisodeTitle) {
+    if (feedRss && !currentTitle) {
       // Populate the dropdown with episode titles
       let titles = useEpisodeTitles(feedRss);
       setTitles(titles);
-      setSelectedRadio('d');
-      setSelectedEpisodeTitle(titles[0] ?? ''); //populate dropdown with most recent episode
+      setCurrentRadio('d');
+      setCurrentTitle(titles[0] ?? ''); //populate dropdown with most recent episode
     }
   }, [feedRss]);
 
   // When the active episode changes, see if the insight button should be enabled
   useEffect(() => {
-    setInsightsEnabled(useEpisodeInsight(selectedEpisodeId) !== strings.NO_INSIGHT);
-  }, [selectedEpisodeId]);
+    setInsightsEnabled(useEpisodeInsight(currentId) !== strings.NO_INSIGHT);
+  }, [currentId]);
 
   useEffect(() => {
-    setEpisodeSeasonString(selectedEpisodeTitle);
-  }, [selectedEpisodeTitle]);
+    updateSelectedEpisodeText(currentTitle);
+  }, [currentTitle]);
 
   // When we change radio OR dropdown options
   useEffect(() => {
-    updateFrameInfo();
-  }, [selectedRadio, selectedEpisodeTitle]);
+    toggleDescriptionOrInsight();
+  }, [currentRadio, currentTitle]);
 
   const [startOpen, setStartOpen] = useState(false);
   const [aboutOpen, setAboutOpen] = useState(true);
@@ -137,28 +140,28 @@ const App = () => {
                 <Fieldset className="left" legend="Episode browser">
                   <Dropdown
                     options={titles}
-                    defaultValue={selectedEpisodeTitle}
-                    onChange={e => setEpisodeSeasonString(e.currentTarget.value) } />
+                    defaultValue={currentTitle}
+                    onChange={e => updateSelectedEpisodeText(e.currentTarget.value) } />
                   <div className="toggle-options" style={{margin: '12px 0 -12px 0'}}>
                     <RadioButton
                       value={'d'}
                       className='row-two'
-                      checked={selectedRadio === 'd'}
-                      onChange={() => setSelectedRadio('d')}>
+                      checked={currentRadio === 'd'}
+                      onChange={() => setCurrentRadio('d')}>
                       Description
                     </RadioButton>
                     <RadioButton
                       value={'i'}
                       className='row-two'
-                      checked={selectedRadio === 'i'}
-                      onChange={() => setSelectedRadio('i')}
+                      checked={currentRadio === 'i'}
+                      onChange={() => setCurrentRadio('i')}
                       disabled={!insightsEnabled}>
                       Insights
                     </RadioButton>
                   </div>
                 </Fieldset>
                 <div className="right">
-                  <Button onClick={() => { loadPlayer(); }}>
+                  <Button onClick={() => { updateUrlForPlayer(); }}>
                     Player
                   </Button>
                 </div>
@@ -225,7 +228,14 @@ const App = () => {
             icon={<img src={paulyImg} width={32} />} />
         )}
         {playerOpen && (
-          <Video w="320" src={selectedEpisodeFile} />
+          <Modal
+            width="310"
+            height="auto"
+            title="Episode player"
+            defaultPosition={{x: 40, y: 350}}
+            closeModal={() => {setPlayerOpen(false)}} >
+            <audio src={currentFile} controls />
+          </Modal>
         )}
       </div>
       <div className="image-container">
